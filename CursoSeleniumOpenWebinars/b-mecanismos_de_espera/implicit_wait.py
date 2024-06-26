2d70187385bbd5747f1c4176587f5027d43da2fd
PaulaNuness <127285238+PaulaNuness@users.noreply.github.com>
2024-06-26 18:48:50 +0200

    26-06-2024__18:48

--- CursoSeleniumOpenWebinars/b-mecanismos_de_espera/implicit_wait.py
+++ CursoSeleniumOpenWebinars/b-mecanismos_de_espera/implicit_wait.py
@@ -1,17 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import time
 
 navegador = webdriver.Chrome()
 
-navegador.implicitly_wait(15)
+navegador.implicitly_wait(10)
 
 navegador.get("http:coniemenezes.com")
 print(navegador.title)
-navegador.implicitly_wait(15)
+time.sleep(5)
 navegador.save_screenshot('print1.png')
 elemento_contato = navegador.find_element(By.LINK_TEXT,"Contato")
-navegador.implicitly_wait(15)
+time.sleep(5)
 elemento_contato.click()
+time.sleep(5)
 navegador.save_screenshot('print2.png')
-navegador.implicitly_wait(15)
+time.sleep(5)
 navegador.quit()
