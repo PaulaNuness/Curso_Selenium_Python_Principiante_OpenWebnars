525cb12981bbd7c219dbc95ed254657fd4376ad6
PaulaNuness <127285238+PaulaNuness@users.noreply.github.com>
2024-07-02 14:11:00 +0200

    02-07-2024__14:10

--- CursoSeleniumOpenWebinars/d-pruebas_con_Unitest/usando_la_libreria_UniTTest.py
+++ CursoSeleniumOpenWebinars/d-pruebas_con_Unitest/usando_la_libreria_UniTTest.py
@@ -1,35 +1,56 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
-# tenemos que ayslar cada una de las pruebas
-# creamos una clase que hereda de unittes
-# defsetUp --> ira ejecutarse al inicio de una prueba
-# deftearDown --> ira ejecutarse al final de una prueba
-# ejemplo:
-#   Ejecutar setUp()
-#   Ejecutar test_uno()
-#   Ejecutar tearDown()
-#   Ejecutar setUp()
-#   Ejecutar test_dos()
-#   Ejecutar tearDown()
-
+import time
 
 class PruebaPython(unittest.TestCase):
 
     def setUp(self):
-        #pasos previos a cada test
+        print("\033[94mEjecuto antes de cada test\033[0m")  # Azul
+        try:
+            # Inicializa o driver do Chrome
+            self.driver = webdriver.Chrome()
 
-    def tearDown(self):
-        #pasos posteriores a cada test
+            # Maximiza a janela do navegador
+            self.driver.maximize_window()
+
+            time.sleep(3)
+            
+        except WebDriverException as e:
+            self.fail(f"\033[91mNo se pudo iniciar el navegador: {e}\033[0m")  # Rojo
+
+########################################################################################################################
 
     def test_uno(self):
-        #test uno
-        #aserción uno
+        print("\033[92mSoy el test 1\033[0m")  # Verde
+        
+        try:
+            
+            # vamos a Google esperamos y pulsamos o botao aceptar
+            self.driver.get("https://www.google.com")
 
-    def test_dos(self):
-        #test dos
-        #aserción dos
+            time.sleep(3)
 
+            accept_button = self.driver.find_element(By.ID, "L2AGLb")
+            accept_button.click()
+    
+            time.sleep(3)
+
+            print("\033[92mTest 1 concluido con suceso!!!\033[0m")  # Verde
+        except Exception as e:
+            self.fail(f"\033[91mEl test 1 falló: {e}\033[0m")  # Rojo
+
+########################################################################################################################
+
+    def tearDown(self):
+        print("\033[94mEjecuto despues de cada test\033[0m")  # Azul
+        time.sleep(3)
+        self.driver.quit()
+
+########################################################################################################################
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(exit=False)
