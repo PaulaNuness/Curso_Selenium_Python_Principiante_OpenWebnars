716d0671759e2070437a5481e6f417edb1cee43c
PaulaNuness <127285238+PaulaNuness@users.noreply.github.com>
2024-07-03 09:27:46 +0200

    03-07-2024__09:27

--- CursoSeleniumOpenWebinars/d-pruebas_con_Unitest/usando_la_libreria_UniTTest.py
+++ CursoSeleniumOpenWebinars/d-pruebas_con_Unitest/usando_la_libreria_UniTTest.py
@@ -59,6 +59,41 @@ class PruebaPython(unittest.TestCase):
 
 ########################################################################################################################
 
+    def test_dos(self):
+        print("\033[92mSoy el test 2\033[0m")  # Verde
+        
+        try:
+            
+            # vamos a Google esperamos y pulsamos o botao aceptar
+            self.driver.get("https://www.google.com")
+
+            time.sleep(3)
+
+            accept_button = self.driver.find_element(By.ID, "L2AGLb")
+            accept_button.click()
+    
+            time.sleep(3)
+
+            # buscamos o campo de busqueda y escrevemos wikipedia
+            busqueda = self.driver.find_element(By.NAME,"q")
+            busqueda.send_keys("wikipedia")
+            time.sleep(2)
+            busqueda.send_keys(Keys.ENTER)
+
+            # vou encontrar o enlace y pulsar 
+            pagina_wikipedia = self.driver.find_element(By.XPATH, '//a[@href="https://es.wikipedia.org/wiki/Wikipedia:Portada"]')
+            pagina_wikipedia.click()
+
+            # ahora vamos comprobar que eu titulo de la pagina es Wikipedia, la enciclopedia libre.
+            titulo = self.driver.title
+            self.assertEqual("Wikipedia, la enciclopedia libre",titulo) # esta correcto
+
+            print("\033[92mTest 2 concluido con suceso!!!\033[0m")  # Verde
+        except Exception as e:
+            self.fail(f"\033[91mEl test 1 falló: {e}\033[0m")  # Rojo
+
+########################################################################################################################
+
     def tearDown(self):
         print("\033[94mEjecuto despues de cada test\033[0m")  # Azul
         time.sleep(3)
